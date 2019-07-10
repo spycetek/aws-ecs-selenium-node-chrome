8c0acb1dd960899d83aa091a70450f009f6292fa
Kanji Furuhashi <pikanji@gmail.com>
2019-07-10 11:59:14 +0800

    Fix entry point script

--- ecs_entry_point.sh
+++ ecs_entry_point.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set REMOTE_HOST to avoid the node registers wrong IP on ECS
-export REMOTE_HOST=http://`ip addr show eth0 | grep -oP \"inet \\K\\S([0-9]+\\.){3}[0-9]+\"`:5555
+export REMOTE_HOST=http://`ip addr show eth0 | grep -oP "inet \K\S([0-9]+\.){3}[0-9]+"`:5555
 
 printenv | grep REMOTE
 
